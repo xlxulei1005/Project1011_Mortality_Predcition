293d3e5805368621e5c0a367e65196b5dd7723a7
Nan Wu <nw1045@nyu.edu>
2017-12-04 19:36:22 -0500

    add note-prediction loss

--- model/Hierarchical_Convolutional_Archetecture_BiGRUmaxpooling/config_Hierachical_BiGRU_max.py
+++ model/Hierarchical_Convolutional_Archetecture_BiGRUmaxpooling/config_Hierachical_BiGRU_max.py
@@ -10,21 +10,21 @@ def config_loading():
 	          'target_class':2, 
 	          'bigru_max_note_hidden': 80, # Number of hidden neurons for the first gru
 	          'batch_size': 16, 
-	          'learning_rate': 0.0001,
+	          'learning_rate': 0.01,
 	          'num_epochs':150,
 	          'cuda': True,
 	          'early_stop': 10,
 	          'val_per_epoch': 1,
 	          'data_portion': 1, # portion of training data to use
-	          'optimizer': 'Adam',
+	          'optimizer': 'SGD',
 	          'padding_before_batch': False, # Only for CNN
 	          'to_sentences': False,  # for sentence and patient level models 
               'word_padded_length_in_notes': None, # only for CNN
               'max_note_length' : 300, # maximun note length (if the maximum length in a minbatch is smaller than this number, no notes will be cropped.)
               'random_cutting': True, # random cropping
-	          'savepath': './model/15m_bigru_max_maxnotelen_300_wordsdim_300_subhidden_200_notehidden_80_batch_16_Adam_0.0001/',
+	          'savepath': './model/15m_bigru_max_regulizer_maxnotelen_300_wordsdim_300_subhidden_200_notehidden_80_batch_16_SGD_0.01/',
 	          'time_name': '15m',
-                  'save_test_result':False
-                'regulization_by_note': True,
+                  'save_test_result':False,
+                'regulization_by_note': True
 	}
 	return config_bigru_max
